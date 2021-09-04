f21d9546722b37872eae641407028678fd3adadf
sritam144 <sbal@asiczen.com>
2021-09-04 16:54:54 +0530

    First commit

---
+++ src/main/java/com/asiczen/test/Arithmatic/ArithmaticApplication.java
@@ -0,0 +1,14 @@
+package com.asiczen.test.Arithmatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmaticApplication.class, args);
+		System.out.println("Hello Maven Central");
+	}
+
+}
---
+++ src/test/java/com/asiczen/test/Arithmatic/ArithmaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiczen.test.Arithmatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
